f9593b3a7aef2c26423601cfb3823a70577f9cd9
Róbert Darida <rdarida@users.noreply.github.com>
2025-09-11 12:12:18 +0200

    feat: implement disable end enable methods for Button class

--- src/ui/Button.ts
+++ src/ui/Button.ts
@@ -1,10 +1,38 @@
 import { Sprite, Texture } from 'pixi.js';
 
+/**
+ * A simple button implementation based on Pixi.Sprite.
+ */
 export class Button extends Sprite {
+  /**
+   * Creates a new button instance with the given texture.
+   *
+   * @param texture The texture to be used for the button sprite.
+   */
   constructor(texture: Texture) {
     super(texture);
 
     this.eventMode = 'static';
     this.cursor = 'pointer';
+  }
+
+  /**
+   * Disable the button, preventing it from receiving pointer events.
+   *
+   * @param hide If set to `true`, the button will also be hidden.
+   *             Defaults to `false`, meaning the button stays visible.
+   */
+  public disable(hide = false): void {
+    this.eventMode = 'none';
+    this.visible = !hide;
+  }
+
+  /**
+   * Enables the button, allowing it to receive pointer events
+   * and making it visible.
+   */
+  public enable(): void {
+    this.eventMode = 'static';
+    this.visible = true;
   }
 }
