bf1d77a11736c5a19460ae415c4fb5afd73decd4
Róbert Darida <rdarida@users.noreply.github.com>
2025-09-28 01:19:34 +0200

    feat: implement Binder.bind, and Binder.unbind methods

--- src/utils/list/Binder.ts
+++ src/utils/list/Binder.ts
@@ -2,20 +2,59 @@ export class Binder {
   protected _prev: Binder;
   protected _next: Binder;
 
+  /**
+   * Gets the previous Binder in the chain.
+   */
   public get prev(): Binder {
     return this._prev;
   }
 
+  /**
+   * Gets the next Binder in the chain.
+   */
   public get next(): Binder {
     return this._next;
   }
 
+  /**
+   * Creates a new Binder instance.
+   *
+   * Each Binder stats as a self-referencing circular node
+   * (`prev = this`, `next = this`);
+   */
   constructor() {
     this._prev = this;
     this._next = this;
   }
 
-  public bind(prev: Binder): void {}
+  /**
+   * Binds the given Binder right after this one in the chain.
+   *
+   * @param next The Binder to insert after the current one.
+   */
+  public bind(next: Binder): void {
+    const a = this;
+    const b = next;
+    const c = this._next;
 
-  public unbind(): void {}
+    a._next = b;
+    b._prev = a;
+
+    if (a === c) return;
+
+    b._next = c;
+    c._prev = b;
+  }
+
+  /**
+   * Unbinds this Binder instance from the chain.
+   */
+  public unbind(): void {
+    const { _prev: a, _next: b } = this;
+
+    a._next = b;
+    b._prev = a;
+    this._prev = this;
+    this._next = this;
+  }
 }
